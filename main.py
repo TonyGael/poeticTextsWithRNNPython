9f35d11858a8163c6e3f6bfad5fa7d8fabc7ff6a
TonyGael <anthonygaelmayol@gmail.com>
2024-03-07 02:03:40 -0300

    agregada la funcion que genera los textos

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ index_to_char = dict((i, c) for i, c in enumerate(characters))
 SEQ_LENGTH = 40
 STEP_SIZE = 3
 
+'''
 sentences = []
 next_char = []
+'''
 
+'''
 for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
     sentences.append(text[i: i + SEQ_LENGTH])
     next_char.append(text[i + SEQ_LENGTH])
@@ -43,5 +46,34 @@ model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
 model.fit(x, y, batch_size=256, epochs=4)
 
 # model.save('textgenerator.model') # save on local folder of the virtual env
+'''
 
 # una vez que se genero el modelo:
+model = tf.keras.models.load('textgenerator.model')
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def generate_text(length, temperature):
+    start_index = random.randint(0, len(text) - SEQ_LENGTH - 1)
+    generated = ''
+    sentence = text[start_index: start_index + SEQ_LENGTH]
+    generated += sentence
+
+    for i in range(length):
+        x_predictions = np.zeros((1, SEQ_LENGTH, len(characters)))
+        for t, char in enumerate(sentence):
+            x_predictions[0, t, char_to_index[char]] = 1
+
+        predictions = model.predict(x_predictions, verbose=0)[0]
+        next_index = sample(predictions, temperature)
+        next_character = index_to_char[next_index]
+
+        generated += next_character
+        sentence = sentence[1:] + next_character
+    return generated
